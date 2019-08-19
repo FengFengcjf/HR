ba2353367ab5c320260840d1424177f19b672f92
FengFengcjf <2592094183@qq.com>
2019-08-19 17:16:34 +0800

    八层提交

---
+++ HR/DAO/StudentDAO.cs
@@ -0,0 +1,81 @@
+﻿using EFentity;
+using IntDAO;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class StudentDAO : DaoBase<Student>, IStudentDAO
+    {
+        public List<StudentModel> SelectBy(StudentModel st)
+        {
+            List<Student> list = SelectBy(e => e.Id.Equals(st.Id));
+            List<StudentModel> list2 = new List<StudentModel>();
+            foreach (var item in list)
+            {
+                StudentModel sd = new StudentModel()
+                {
+                    Id =item.Id,
+                    Name=item.Name
+                };
+                list2.Add(sd);
+            }
+            return list2;
+        }
+
+        public int StudentAdd(StudentModel st)
+        {
+            //把DTO转为EO
+            Student est = new Student()
+            {
+                Id = st.Id,
+                Name = st.Name
+            };
+            return Add(est);
+        }
+
+        public int StudentDel(Model.StudentModel st)
+        {
+            Student est = new Student()
+            {
+                Id = st.Id
+
+            };
+            return Delete(est);
+        }
+
+        public List<Model.StudentModel> StudentSelect()
+        {
+            List<Student> list = SelectAll();
+            List<StudentModel> list2 = new List<StudentModel>();
+            foreach (Student item in list)
+            {
+                StudentModel sm = new StudentModel()
+                {
+                    Id = item.Id,
+                    Name = item.Name
+                };
+                list2.Add(sm);
+            }
+            return list2;
+        }
+
+        public int StudentUpdate(StudentModel st)
+        {
+            Student est = new Student()
+            {
+                Id = st.Id,
+                Name = st.Name
+            };
+            return Update(est);
+        }
+
+   
+
+      
+    }
+}
---
+++ HR/IOC/IocCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using lntBLL;
+using IntDAO;
+using DAO;
+using System.Configuration;
+using Microsoft.Practices.Unity.Configuration;
+namespace IOC
+{
+    public class IocCreate
+    {
+        public static IStudentDAO CreateStudenDAO()
+        {
+            UnityContainer ioc = new UnityContainer();
+            ioc.RegisterType<IStudentDAO, StudentDAO>();
+            return ioc.Resolve<IStudentDAO>();
+        }
+
+        public static IStudentBLL CreateStudentBLL()
+        {
+            UnityContainer ioc = GetBLLSeciton();
+            return ioc.Resolve<IStudentBLL>("StudentBLL");
+        }
+
+        private static UnityContainer GetBLLSeciton()
+        {
+            UnityContainer ioc = new UnityContainer();
+            ExeConfigurationFileMap ecf = new ExeConfigurationFileMap();
+            ecf.ExeConfigFilename = @"D:\VS\阶段四\HR\UI\Unity.config";
+            Configuration cf = ConfigurationManager.OpenMappedExeConfiguration(ecf, ConfigurationUserLevel.None);
+            UnityConfigurationSection cfs = cf.GetSection("unity") as UnityConfigurationSection;
+            ioc.LoadConfiguration(cfs, "containerTwo");
+            return ioc;
+        }
+    }
+}
