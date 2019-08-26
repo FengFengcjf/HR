e6386ae4bfe5d4272a7b1875d6c1290b55b4d736
FengFengcjf <2592094183@qq.com>
2019-08-26 10:16:27 +0800

    合并后第一次提交

---
+++ HR/lntBLL/Iconfig_major_kindBLL.cs
@@ -0,0 +1,19 @@
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lntBLL
+{
+    public interface Iconfig_major_kindBLL
+    {
+        int Add(config_major_kindModel st);
+        int Del(config_major_kindModel st);
+        int Update(config_major_kindModel st);
+        List<config_major_kindModel> Select();
+            List<config_major_kindModel> SelectBy(config_major_kindModel st);
+    }
+}
+
